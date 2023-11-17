47ba3a6d4185b3442b211f309287b21e3343b154
brianochienm <144845238+brianochienm@users.noreply.github.com>
2023-11-17 21:42:07 +0300

    Update DbConn.php

--- configs/DbConn.php
+++ configs/DbConn.php
@@ -1,13 +1,12 @@
 <?php
-
-require_once "constants.php";
-try{
-    $pdo = new PDO ("mysql:host=$hostname; 
-    dbname=$dbname", $username, $userpass);
-    $pdo-> setattribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-   // echo "connected successfully!!";
-}catch(PDOException $e){
-    echo "connection failed" . $e-> getmessage();
-}
-
-?>
+   require_once "constants.php";
+   try{
+       $pdo = new PDO ("mysql:host=$hostname; 
+       dbname=$dbname", $username, $userpass);
+       $pdo-> setattribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      // echo "connected successfully!!";
+   }catch(PDOException $e){
+       echo "connection failed" . $e-> getmessage();
+   }
+   
+   ?>
