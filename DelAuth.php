4e0f42337c4932116caaee6dc7cb541d8da85443
brianochienm <144845238+brianochienm@users.noreply.github.com>
2023-11-17 21:40:30 +0300

    Update DelAuth.php

--- DelAuth.php
+++ DelAuth.php
@@ -1,23 +1,22 @@
 <?php
-
-require_once "configs/DbConn.php";
-
-if (isset($_GET["DelId"])) {
-    $authorId = $_GET["DelId"];  // Sanitize or validate the input if needed
-
-    $stmt = $pdo->prepare("SELECT * FROM authorstb WHERE AuthorId=? LIMIT 1");
-    $stmt->execute([$authorId]);
-    $author = $stmt->fetch();
-
-    if ($author) {
-        $stmt = $pdo->prepare("DELETE FROM authorstb WHERE AuthorId=?");
-        $stmt->execute([$authorId]);
-
-        header("Location: ViewAuthors.php");
-        exit();
-    } else {
-        header("Location: ViewAuthors.php");
-        exit();
-    }
-}
-?>
+   require_once "configs/DbConn.php";
+   
+   if (isset($_GET["DelId"])) {
+       $authorId = $_GET["DelId"];  // Sanitize or validate the input if needed
+   
+       $stmt = $pdo->prepare("SELECT * FROM authorstb WHERE AuthorId=? LIMIT 1");
+       $stmt->execute([$authorId]);
+       $author = $stmt->fetch();
+   
+       if ($author) {
+           $stmt = $pdo->prepare("DELETE FROM authorstb WHERE AuthorId=?");
+           $stmt->execute([$authorId]);
+   
+           header("Location: ViewAuthors.php");
+           exit();
+       } else {
+           header("Location: ViewAuthors.php");
+           exit();
+       }
+   }
+   ?>
